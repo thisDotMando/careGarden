22a492ba6f3c70bbca71e879f5ea42d05745ee54
Michel Kowal <michelkowal@hotmail.de>
2025-10-20 11:43:00 +0000

    Deine Commit-Nachricht hier

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
         <p className="small">
-          Edit <code>careGarden</code> and save to reload.
+          Edit <code>careGarden.netlify</code> and save to reload.
         </p>
         <p>
           <a
